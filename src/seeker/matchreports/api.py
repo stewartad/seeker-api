8481c1c0579f4de6c8f7b26bbb7f3b2127e9ed57
Austen <adstewart@email.arizona.edu>
2021-06-03 00:31:16 -0700

    extract leaderboard queryset creation to a func

--- src/seeker/matchreports/api.py
+++ src/seeker/matchreports/api.py
@@ -5,6 +5,7 @@ from rest_framework import serializers, viewsets, permissions
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.response import Response
 from . import models
+from . import views
 
 def setup_eager_loading(get_queryset):
     def decorator(self):
@@ -77,11 +78,5 @@ class LeaderboardViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     def list(self, request):
         guild = request.query_params.get('guild_id')
-
-        reports = models.Report.objects.filter(user=OuterRef('user_id')).values('user_id').annotate(game_wins=Sum('games')).values('game_wins')[:1]
-        queryset = models.User.objects.filter(report__match__guild=guild) \
-            .annotate(won_games=Subquery(reports)) \
-            .annotate(total_games=Sum('report__match__reports__games')) \
-            .order_by('-total_games')
-        serializer = LeaderboardSerializer(queryset, many=True)
+        serializer = LeaderboardSerializer(views.get_leaderboard(guild), many=True)
         return Response(serializer.data)
--- src/seeker/matchreports/views.py
+++ src/seeker/matchreports/views.py
@@ -1,13 +1,21 @@
-from rest_framework.views import APIView
-from .models import Guild, Match
+from django.db.models.aggregates import Sum
+from django.db.models.expressions import OuterRef, Subquery
+from . import models
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
+def get_leaderboard(guild):
+    # TODO: Filter by date
+    reports = models.Report.objects.filter(user=OuterRef('user_id')).values('user_id').annotate(game_wins=Sum('games')).values('game_wins')[:1]
+    queryset = models.User.objects.filter(report__match__guild=guild) \
+        .annotate(won_games=Subquery(reports)) \
+        .annotate(total_games=Sum('report__match__reports__games')) \
+        .order_by('-total_games')
+    return queryset
 
 # Create your views here.
 def index(request):
-    latest_match_list = Match.objects.order_by('-date')[:20]
+    latest_match_list = models.Match.objects.order_by('-date')[:20]
     context = {'latest_match_list': latest_match_list}
     return render(request, 'matchreports/index.html', context)
 
