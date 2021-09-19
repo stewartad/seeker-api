bf70151abe12e61c906e71b281a76ff68c7872f6
stewartad <austendean@gmail.com>
2021-09-18 17:35:57 -0700

    preparing for deploy

--- src/seeker/seeker/settings.py
+++ src/seeker/seeker/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.yequari.com']
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 
 # Application definition
